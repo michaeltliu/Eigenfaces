5bd581776d1f3c9c54a7ae5583abe5d7acc2991b
Danny Luo <zeping.luo@duke.edu>
2019-11-28 00:42:45 -0500

    pca

--- Eigenfaces.R
+++ Eigenfaces.R
@@ -5,14 +5,15 @@ library(jpeg)
 library(imager)
 library(spatstat)
 library(ggfortify)
+library(ggrepel) 
 ##read in jpeg
 prepath <- "/Users/zepingluo/Documents/Eigenfaces/faceimages/"
-filename <- c("IMG_0733.JPG","IMG_0734.JPG","IMG_0735.JPG","IMG_0749.JPG","IMG_0753.JPG","IMG_0755.JPG","IMG_0759.JPG","IMG_0772.JPG")
+filename <- c("IMG_0733.JPG","IMG_0734.JPG","IMG_0735.JPG","IMG_0736.JPG","IMG_0749.JPG","IMG_0753.JPG","IMG_0755.JPG","IMG_0759.JPG","IMG_0772.JPG")
 paths <- c()
 images <- c()
 vecs <- c()
 # bind all vectors into a single r*c by 20ish matrix *vecs*
-for(index in 1:8){
+for(index in 1:9){
   #collect the paths in the vector
   paths <- c(paths,paste(prepath,filename[index],sep=""))
   #read single filename
@@ -52,7 +53,7 @@ mean_vec <- c()
 mean_vec <- apply(vecs,1,mean)
 dim(mean_vec)
 X_mean <- matrix(, nrow = 921600, ncol = 0)
-for(i in 1:8){
+for(i in 1:9){
   X_mean <- cbind(X_mean, mean_vec)
 }
 dim(X_mean)
@@ -66,23 +67,37 @@ vecs <- vecs-X_mean
 # matrix. Use the small rank of the data matrix to simplify
 # eigen computations
 
-pca <- prcomp(t(vecs))
-plot(pca)
-colnames(vecs) <- c(1:8)
-#long runtime??
-autoplot(pca, data = t(vecs),  loadings = TRUE,loadings.label=TRUE)
 
 # -----------------------------------
 
 # choose top 2 or 3 principal components (for visuals)
-proj_matrix <- pca$rotation[,0:2]
+eig <- eigen(t(vecs)%*%vecs)
+eigenvectors <- matrix(, nrow = 921600, ncol = 0)
+for(j in 1:9){
+  #from https://medium.com/@devalshah1619/face-recognition-using-eigenfaces-technique-f221d505d4f7
+  #using data matrix times eigen of (transpose data) * (data)
+  eigenvector <- vecs%*%eig$vectors[,j]
+  eigenvectors <- cbind(eigenvectors,eigenvector)
+}
+dim(eigenvectors)
+#maybe we need to rank eigenvectors first?
+proj_matrix <- eigenvectors[,0:2]
 dim(proj_matrix)
 dim(vecs)
 
 # and choose top 5 principal components (for more accurate analysis)
 # and project each original face vector onto these principal components
-result <- proj_matrix%*%vecs
+result <- t(proj_matrix)%*%vecs
+dim(result)
 
+#plot
+
+table <- as.table(t(result))
+tag <- c("Danny","Danny","Danny","Danny","Jane","Jane","Jane","Jane","Jane")
+table <- cbind(table,tag)
+colnames(table) <- c("pc1","pc2","label")
+p <- ggplot(data=table,aes(x=pc1,y=pc2,label=label))
+p+geom_point()+geom_text_repel()
 # -------------------------------------
 
 # take a new face vector and project onto principal components
