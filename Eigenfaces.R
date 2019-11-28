a079540654ff23f16eaefb08fc53fdb985d57258
Danny Luo <zeping.luo@duke.edu>
2019-11-28 01:21:22 -0500

    c

--- Eigenfaces.R
+++ Eigenfaces.R
@@ -7,13 +7,17 @@ library(spatstat)
 library(ggfortify)
 library(ggrepel) 
 ##read in jpeg
+#length is the number of images we used  - called upon multiple times later in the files
+length <- 19
 prepath <- "/Users/zepingluo/Documents/Eigenfaces/faceimages/"
-filename <- c("IMG_0733.JPG","IMG_0734.JPG","IMG_0735.JPG","IMG_0736.JPG","IMG_0749.JPG","IMG_0753.JPG","IMG_0755.JPG","IMG_0759.JPG","IMG_0772.JPG")
+filename <- c("Danny1.JPG","Danny2.JPG","Danny3.JPG","Danny4.JPG","Jane1.JPG","Jane2.JPG","Jane3.JPG",
+              "Jane4.JPG","Jane5.JPG","Andy1.JPG","Andy2.JPG","Andy3.JPG","Andy4.JPG","Andy5.JPG",
+              "Jessica1.JPG","Jessica2.JPG","Jessica3.JPG","Jessica4.JPG","Jessica5.JPG")
 paths <- c()
 images <- c()
 vecs <- c()
 # bind all vectors into a single r*c by 20ish matrix *vecs*
-for(index in 1:9){
+for(index in 1:length){
   #collect the paths in the vector
   paths <- c(paths,paste(prepath,filename[index],sep=""))
   #read single filename
@@ -31,19 +35,6 @@ for(index in 1:9){
 dim(vecs)
 
 
-##code for onvert matrix back to image
-image <- im(as.matrix(grayscale))
-plot(image)
-
-
-#dim(gray)
-#https://stackoverflow.com/questions/31800687/how-to-get-a-pixel-matrix-from-grayscale-image-in-r/31804561
-#https://www.tutorialspoint.com/dip/grayscale_to_rgb_conversion.htm
-#matrix <- 1/3*x[,,1]+1/3*x[,,2]+1/3*x[,,3]
-#dim(matrix)
-
-
-
 # ------------------------------
 
 # find the mean vector then zero out/subtract the mean from 
@@ -53,7 +44,7 @@ mean_vec <- c()
 mean_vec <- apply(vecs,1,mean)
 dim(mean_vec)
 X_mean <- matrix(, nrow = 921600, ncol = 0)
-for(i in 1:9){
+for(i in 1:length){
   X_mean <- cbind(X_mean, mean_vec)
 }
 dim(X_mean)
@@ -73,7 +64,7 @@ vecs <- vecs-X_mean
 # choose top 2 or 3 principal components (for visuals)
 eig <- eigen(t(vecs)%*%vecs)
 eigenvectors <- matrix(, nrow = 921600, ncol = 0)
-for(j in 1:9){
+for(j in 1:length){
   #from https://medium.com/@devalshah1619/face-recognition-using-eigenfaces-technique-f221d505d4f7
   #using data matrix times eigen of (transpose data) * (data)
   eigenvector <- vecs%*%eig$vectors[,j]
@@ -93,7 +84,7 @@ dim(result)
 #plot
 
 table <- as.table(t(result))
-tag <- c("Danny","Danny","Danny","Danny","Jane","Jane","Jane","Jane","Jane")
+tag <- filename
 table <- cbind(table,tag)
 colnames(table) <- c("pc1","pc2","label")
 p <- ggplot(data=table,aes(x=pc1,y=pc2,label=label))
@@ -105,10 +96,14 @@ p+geom_point()+geom_text_repel()
 
 #--------------------------
 #visualzie eigen faces
+par(mfrow=c(2,2))
+for(e in 1:4){
+  image_matrix <- matrix(eigenvectors[,e],nrow = 960)
+  dim(image_matrix)
+  image <- im(image_matrix)
+  plot(image,main=paste(paste("top ",e),"eigen faces"))
+}
 
-
-image <- im(matrix(eigenvectors[,1],nrow = 960))
-plot(image)
 
 
 
